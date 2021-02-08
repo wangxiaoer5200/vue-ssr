89c6515cf77880f0fecf40693c76844a09c2aef4
Xinhua <xhyou@hitotek.com>
2021-02-08 16:00:39 +0800

    ..

--- server.js
+++ server.js
@@ -4,18 +4,35 @@ const express = require('express')
 const fs = require('fs')
 
 const { createBundleRenderer } = require('vue-server-renderer')
+const setupDevServer = require('./build/setup-dev-server')
 
 const server = express()
 // express.static处理物理磁盘文件
 server.use('/dist', express.static('./dist'))
 
-const serverBundle = require('./dist/vue-ssr-server-bundle.json')
-const clientManifest = require('./dist/vue-ssr-client-manifest.json')
-const template = fs.readFileSync('./index.template.html', 'utf-8')
-const renderer = createBundleRenderer(serverBundle, {
-    template,
-    clientManifest
-})
+// 判断是否是开发模式
+const isProd = process.env.NODE_ENV === 'production'
+
+let renderer
+let onReady // 返回一个promise对象，方便以后调用
+if (isProd) {
+    // 引入 vue-server-renderer
+    const serverBundle = require('./dist/vue-ssr-server-bundle.json')
+    const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+    const template = fs.readFileSync('./index.template.html', 'utf-8')
+    const renderer = createBundleRenderer(serverBundle, {
+        template,
+        clientManifest
+    })
+}   else {
+    // 等待构建 => 重新生成renderer
+    onReady = setupDevServer(server, (serverBundle, template, clientManifest) => {
+        renderer = createBundleRenderer(serverBundle, {
+            template,
+            clientManifest
+        })
+    })
+}
 
 const render = async (req, res) => {
     try {
@@ -29,7 +46,14 @@ const render = async (req, res) => {
     }
 } 
 
-server.get('*', render)
+// 服务端路由为 * ，意味着所有路由都会进入
+server.get('*', isProd ? 
+render 
+: async (req, res) => {
+    // 等待有了renderer渲染器后，调用render渲染
+    await onReady
+    render(req, res)
+})
 
 server.listen(3000, () => console.log('place is http://localhost:3000'))
 
